af75e429fcaa52c294716c8f44124fe1c38299d3
Rodrigo Martín Prieto <r.martin1@udc.es>
2018-08-06 19:22:18 +0200

    Unified MB2 and MB3 classes under a generic factory and implemented shell to work with both MB2 and MB3, API and other services still pending

--- lib/service_config_export.py
+++ lib/service_config_export.py
@@ -3,15 +3,17 @@
 # This file is provided in the repo, but you might want to update it
 
 from miband2 import MiBand2
+from miband3 import MiBand3
 import json
 
-ADDR = "00:11:22:33:44:55"
+#ADDR = "00:11:22:33:44:55"
+ADDR = "db:f7:85:30:32:03"
 
-mb2 = MiBand2(ADDR, initialize=true)
+mb = MiBand3(ADDR, 'abcdefghijklmnop', initialize=True)
 
 svc_export = {}
 
-for svc in mb2.svcs:
+for svc in mb.svcs:
     svc_export[str(svc.uuid)] = {"hndStart": svc.hndStart, "hndEnd": svc.hndEnd, "chars":{}}
     for c in svc.getCharacteristics():
         svc_export[str(svc.uuid)]["chars"][str(c.uuid)] = {"handle":c.handle, "valHandle":c.valHandle, "properties":c.properties, "descs":{}}
@@ -19,5 +21,5 @@ for svc in mb2.svcs:
             svc_export[str(svc.uuid)]["chars"][str(c.uuid)]["descs"][str(d.uuid)] = {"handle": d.handle}
     print "SERVICE DONE"
 
-with open('mb2services.json', 'w') as outfile:
+with open('mb3services.json', 'w') as outfile:
     json.dump(svc_export, outfile)
